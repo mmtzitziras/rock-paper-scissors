1e26087f9fafe16cda8bc4b978bcfbc9d813ddfd
mmtzitziras <mariostzitziras4@gmail.com>
2024-04-18 13:53:47 +0300

    Created function to play a single round of the game and return the result

--- js/main.js
+++ js/main.js
@@ -21,5 +21,24 @@ function getPlayerChoice(){
     return input;
 }
 
+function playRound(computerChoice, playerChoice){
+    if (computerChoice === playerChoice) {
+        return "It's a tie";
+    }
+    else if (computerChoice === "rock" && playerChoice === "scissors") {
+        return "You lose! Rock beats Scissors";
+    }
+    else if (computerChoice === "paper" && playerChoice === "rock") {
+        return "You lose! Paper beats Rock";
+    }
+    else if (computerChoice === "scissors" && playerChoice === "paper") {
+        return "You lose! Scissors beats Paper";
+    }
+    else {
+        return "You win! " + computerChoice + " beats " + playerChoice;
+    }
+}
+
+
 console.log(getComputerChoice());
 console.log(getPlayerChoice());
